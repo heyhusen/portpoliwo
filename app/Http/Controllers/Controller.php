2d76f7510216784f1c51b569ce5d1a66075f69ad
Ahmad Husen <husenisme@gmail.com>
2020-06-12 23:16:14 +0700

    Add global responser

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Datakrama\Lapires\Traits\ApiResponser;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -9,5 +10,37 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ApiResponser;
+
+    /**
+     * Data created response
+     *
+     * @param array $data
+     * @return void
+     */
+    public function dataCreated($data)
+    {
+        return $this->successResponse($data, __('Data successfully created.'), 201);
+    }
+
+    /**
+     * Data updated response
+     *
+     * @param array $data
+     * @return void
+     */
+    public function dataUpdated($data)
+    {
+        return $this->successResponse($data, __('Data successfully updated.'));
+    }
+
+    /**
+     * Data deleted response
+     *
+     * @return void
+     */
+    public function dataDeleted()
+    {
+        return $this->successResponse(null, __('Data successfully deleted.'));
+    }
 }

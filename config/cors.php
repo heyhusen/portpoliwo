5166c1a5eb2e8eeef1426618661500dd5aa64f17
Ahmad Husen <husenisme@gmail.com>
2020-07-21 21:31:16 +0700

    Update CORS config

--- config/cors.php
+++ config/cors.php
@@ -31,7 +31,7 @@ return [
     /*
      * Matches the request origin. `[*]` allows all origins.
      */
-    'allowed_origins' => ['http://husenis.me', 'https://husenis.me', '*.husenis.me'],
+    'allowed_origins' => ['*.husenis.me'],
 
     /*
      * Matches the request origin with, similar to `Request::is()`

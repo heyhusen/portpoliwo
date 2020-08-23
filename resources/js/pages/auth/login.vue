57cd37351063246e8a87bd108baa5bc3352afd69
Ahmad Husen <husenisme@gmail.com>
2020-08-23 10:37:49 +0700

    Add top border to login page

--- resources/js/pages/auth/login.vue
+++ resources/js/pages/auth/login.vue
@@ -1,55 +1,69 @@
 <template>
-  <section class="hero is-bold is-fullheight" type="is-light">
-    <div class="hero-body">
-      <div class="container is-fullhd">
-        <div class="columns">
-          <div class="column is-one-third is-offset-one-third">
-            <div class="box">
-              <div class="section has-text-centered">
-                <router-link :to="{ name: 'home' }" title="Portpoliwo"
-                  ><h1 class="title">Portpoliwo</h1></router-link
-                >
-              </div>
-              <ValidationObserver ref="form" v-slot="{ passes }">
-                <form @submit.prevent="passes(onSubmit)">
-                  <div class="columns is-multiline">
-                    <div class="column is-12">
-                      <FormInput
-                        v-model="user.email"
-                        label="E-Mail"
-                        name="email"
-                        icon="email"
-                      />
-                    </div>
-                    <div class="column is-12">
-                      <FormInput
-                        v-model="user.password"
-                        label="Password"
-                        name="password"
-                        type="password"
-                        icon="lock"
-                      />
-                    </div>
-                    <div class="column is-12">
-                      <b-field>
-                        <b-button
-                          type="is-primary"
-                          native-type="submit"
-                          rounded
-                          expanded
-                          >Log in</b-button
-                        >
-                      </b-field>
-                    </div>
+  <div>
+    <div class="has-background-primary pt-1"></div>
+    <section class="hero is-bold" type="is-light">
+      <div class="hero-body">
+        <div class="container is-fullhd">
+          <div class="columns">
+            <div class="column is-three-fifths is-offset-one-fifth">
+              <div class="box">
+                <div class="columns is-vcentered">
+                  <div class="column is-half has-text-centered">
+                    <router-link :to="{ name: 'home' }" title="Portpoliwo"
+                      ><h1 class="title is-size-1">{{ title }}</h1></router-link
+                    >
                   </div>
-                </form>
-              </ValidationObserver>
+                  <div class="column is-half">
+                    <div class="section has-text-centered">
+                      <h1 class="title">Sign in</h1>
+                    </div>
+                    <ValidationObserver ref="form" v-slot="{ passes }">
+                      <form @submit.prevent="passes(onSubmit)">
+                        <div class="columns is-multiline">
+                          <div class="column is-12">
+                            <FormInput
+                              v-model="user.email"
+                              size="is-medium"
+                              label="E-Mail"
+                              name="email"
+                              icon="email"
+                            />
+                          </div>
+                          <div class="column is-12">
+                            <FormInput
+                              v-model="user.password"
+                              size="is-medium"
+                              label="Password"
+                              name="password"
+                              type="password"
+                              icon="lock"
+                            />
+                          </div>
+                          <div class="column is-12">
+                            <b-field>
+                              <b-button
+                                size="is-medium"
+                                type="is-primary"
+                                native-type="submit"
+                                expanded
+                                >Log in</b-button
+                              >
+                            </b-field>
+                          </div>
+                        </div>
+                      </form>
+                    </ValidationObserver>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+
+    <Footer />
+  </div>
 </template>
 
 <script>
@@ -61,8 +75,12 @@ export default {
   metaInfo: {
     title: 'Log in',
   },
+  components: {
+    Footer: () => import('@/js/components/themes/Footer'),
+  },
   data() {
     return {
+      title: process.env.MIX_APP_NAME,
       user: {
         email: '',
         password: '',

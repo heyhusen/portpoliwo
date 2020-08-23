82b2be7f1895d4b04636438047d18d4792d37c9e
Ahmad Husen <husenisme@gmail.com>
2020-08-23 10:36:27 +0700

    Add top border to Navbar

--- resources/js/components/themes/Navbar.vue
+++ resources/js/components/themes/Navbar.vue
@@ -1,46 +1,49 @@
 <template>
-  <b-navbar wrapper-class="container is-fullhd" type="is-white">
-    <template slot="brand">
-      <b-navbar-item tag="router-link" :to="{ name: 'home' }">
-        <strong>Portpoliwo</strong>
-      </b-navbar-item>
-    </template>
-    <template slot="end">
-      <b-dropdown position="is-bottom-left" append-to-body aria-role="menu">
-        <a slot="trigger" class="navbar-item" role="button">
-          <span>Menu</span>
-          <b-icon icon="menu-down"></b-icon>
-        </a>
+  <div>
+    <div class="has-background-primary pt-1"></div>
+    <b-navbar wrapper-class="container is-fullhd" type="is-white">
+      <template slot="brand">
+        <b-navbar-item tag="router-link" :to="{ name: 'home' }">
+          <strong>Portpoliwo</strong>
+        </b-navbar-item>
+      </template>
+      <template slot="end">
+        <b-dropdown position="is-bottom-left" append-to-body aria-role="menu">
+          <a slot="trigger" class="navbar-item" role="button">
+            <span>Menu</span>
+            <b-icon icon="menu-down"></b-icon>
+          </a>
 
-        <b-dropdown-item custom aria-role="menuitem">
-          <div class="level">
-            <div class="level-item">
-              <figure class="image is-96x96">
-                <img class="is-rounded" :src="user.avatar" />
-              </figure>
+          <b-dropdown-item custom aria-role="menuitem">
+            <div class="level">
+              <div class="level-item">
+                <figure class="image is-96x96">
+                  <img class="is-rounded" :src="user.avatar" />
+                </figure>
+              </div>
             </div>
-          </div>
-          Logged as <b>{{ user.name }}</b>
-        </b-dropdown-item>
-        <hr class="dropdown-divider" />
-        <b-dropdown-item has-link aria-role="menuitem">
-          <router-link :to="{ name: 'account-me' }">
-            <b-icon icon="account"></b-icon>
-            My Account
-          </router-link>
-        </b-dropdown-item>
-        <hr class="dropdown-divider" aria-role="menuitem" />
-        <b-dropdown-item value="settings">
-          <b-icon icon="cog"></b-icon>
-          Settings
-        </b-dropdown-item>
-        <b-dropdown-item aria-role="menuitem" @click="signOut()">
-          <b-icon icon="logout"></b-icon>
-          Logout
-        </b-dropdown-item>
-      </b-dropdown>
-    </template>
-  </b-navbar>
+            Logged as <b>{{ user.name }}</b>
+          </b-dropdown-item>
+          <hr class="dropdown-divider" />
+          <b-dropdown-item has-link aria-role="menuitem">
+            <router-link :to="{ name: 'account-me' }">
+              <b-icon icon="account"></b-icon>
+              My Account
+            </router-link>
+          </b-dropdown-item>
+          <hr class="dropdown-divider" aria-role="menuitem" />
+          <b-dropdown-item value="settings">
+            <b-icon icon="cog"></b-icon>
+            Settings
+          </b-dropdown-item>
+          <b-dropdown-item aria-role="menuitem" @click="signOut()">
+            <b-icon icon="logout"></b-icon>
+            Logout
+          </b-dropdown-item>
+        </b-dropdown>
+      </template>
+    </b-navbar>
+  </div>
 </template>
 
 <script>

4e34b3aa1b85ae856fef7f8a9dabfb08b15d83b9
Ahmad Husen <husenisme@gmail.com>
2020-08-27 21:28:51 +0700

    Set Page to use media-library

--- app/Models/Blog/Page.php
+++ app/Models/Blog/Page.php
@@ -4,10 +4,15 @@ namespace App\Models\Blog;
 
 use Datakrama\Eloquid\Traits\Uuids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Page extends Model
+class Page extends Model implements HasMedia
 {
-    use Uuids;
+    use Uuids, InteractsWithMedia, SoftDeletes;
 
     /**
      * The table associated with the model.
@@ -21,5 +26,49 @@ class Page extends Model
      *
      * @var array
      */
-    protected $fillable = ['title', 'slug', 'content', 'image'];
+    protected $fillable = ['title', 'slug', 'content'];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = ['thumbnail'];
+
+    /**
+     * Get the user's full name.
+     *
+     * @return string
+     */
+    public function getThumbnailAttribute()
+    {
+        if ($this->getFirstMedia('thumbnail')) {
+            return $this->getFirstMediaUrl('thumbnail', 'thumb');
+        }
+        return 'thumbnail';
+    }
+
+    /**
+     * Register media collections
+     *
+     * @return void
+     */
+    public function registerMediaCollections(): void
+    {
+        $this
+            ->addMediaCollection('thumbnail')
+            ->singleFile();
+    }
+
+    /**
+     * Register media conversions
+     *
+     * @param Media $media
+     * @return void
+     */
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('thumb')
+                ->fit(Manipulations::FIT_CROP, 512, 512);
+    }
 }
---
+++ database/migrations/2020_08_27_212334_remove_image_column_from_blog_pages_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemoveImageColumnFromBlogPagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('blog_pages', function (Blueprint $table) {
+            $table->dropColumn('image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('blog_pages', function (Blueprint $table) {
+            $table->string('image')->after('content');
+        });
+    }
+}

cebc4faf88cd39901e5c5c7519139aaae5fe67a4
Ahmad Husen <husenisme@gmail.com>
2020-09-03 20:38:53 +0700

    Add blog/tag test

---
+++ tests/Feature/Blog/TagModuleTest.php
@@ -0,0 +1,309 @@
+<?php
+
+namespace Tests\Feature\Blog;
+
+use App\Models\Blog\Tag;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
+use Tests\Feature\Auth;
+use Tests\TestCase;
+
+class TagModuleTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $table = 'blog_tags';
+    protected $url = '/api/blog/tag';
+
+    /**
+     * Test creating a record
+     *
+     * @return void
+     */
+    public function testCreateBlogTag()
+    {
+        factory(Tag::class)->create([
+            'title' => 'Laravel',
+            'slug' => 'laravel',
+            'description' => 'Open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller architectural pattern and based on Symfony.'
+        ]);
+
+        $this
+            ->assertDatabaseCount($this->table, 1)
+            ->assertDatabaseHas($this->table, [
+                'title' => 'Laravel',
+                'slug' => 'laravel',
+                'description' => 'Open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller architectural pattern and based on Symfony.'
+            ]);
+    }
+
+    /**
+     * Test updating a record
+     *
+     * @return void
+     */
+    public function testUpdateBlogTag()
+    {
+        factory(Tag::class)->create();
+
+        $blogTag = Tag::first();
+        $blogTag->fill([
+            'title' => 'Laravel',
+            'slug' => 'laravel',
+            'description' => 'Open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller architectural pattern and based on Symfony.'
+        ]);
+        $blogTag->save();
+
+        $this
+            ->assertDatabaseCount($this->table, 1)
+            ->assertDatabaseHas($this->table, [
+                'title' => 'Laravel',
+                'slug' => 'laravel',
+                'description' => 'Open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller architectural pattern and based on Symfony.'
+            ]);
+    }
+
+    /**
+     * Test deleting a record
+     *
+     * @return void
+     */
+    public function testDeleteBlogTag()
+    {
+        factory(Tag::class)->create();
+
+        $blogTag = Tag::first();
+        $blogTag->delete();
+
+        $this->assertDeleted($blogTag);
+    }
+
+    /**
+     * Test creating a record through API with validation
+     *
+     * @return void
+     */
+    public function testFailedCreateBlogTagFromApi()
+    {
+        $auth = new Auth();
+        $auth->createAuth();
+
+        $response = $this->postJson($this->url);
+
+        $response
+            ->assertStatus(422)
+            ->assertJson([
+                'success' => false,
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    'title' => ['The title field is required.']
+                ]
+            ]);
+    }
+
+    /**
+     * Test creating a record through API
+     *
+     * @return void
+     */
+    public function testSuccessfullCreateBlogTagFromApi()
+    {
+        $auth = new Auth();
+        $auth->createAuth();
+
+        $response = $this->postJson($this->url, [
+            'title' => 'Vue.js'
+        ]);
+
+        $response
+            ->assertCreated()
+            ->assertJson([
+                'success' => true,
+                'message' => 'Data successfully created.',
+                'data' => [
+                    'title' => 'Vue.js',
+                    // 'slug' => 'vuejs'
+                ]
+            ]);
+
+        $response = $this->postJson($this->url, [
+            'title' => 'Laravel',
+            'slug' => 'laravel',
+            'description' => 'Open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller architectural pattern and based on Symfony.'
+        ]);
+
+        $response
+            ->assertCreated()
+            ->assertJson([
+                'success' => true,
+                'message' => 'Data successfully created.',
+                'data' => [
+                    'title' => 'Laravel',
+                    'slug' => 'laravel',
+                    'description' => 'Open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller architectural pattern and based on Symfony.'
+                ]
+            ]);
+
+        $response = $this->postJson($this->url, [
+            'title' => 'Laravel',
+            'slug' => 'laravel',
+            'description' => 'Open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller architectural pattern and based on Symfony.'
+        ]);
+
+        $this->assertDatabaseCount($this->table, 2);
+
+        $response
+            ->assertStatus(422)
+            ->assertJson([
+                'success' => false,
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    'title' => ['The title has already been taken.'],
+                    'slug' => ['The slug has already been taken.']
+                ]
+            ]);
+    }
+
+    /**
+     * Test failed reading an existing record through API
+     *
+     * @return void
+     */
+    public function testFailedReadBlogTagFromApi()
+    {
+        $auth = new Auth();
+        $auth->createAuth();
+
+        $uuid = Str::uuid();
+
+        $response = $this->getJson($this->url . '/' . $uuid);
+
+        $response
+            ->assertStatus(404)
+            ->assertJson([
+                'success' => false,
+                'message' => 'No query results for model [App\\Models\\Blog\Tag] ' . $uuid
+            ]);
+    }
+
+    /**
+     * Test reading an existing record through API
+     *
+     * @return void
+     */
+    public function testSuccessfullReadBlogTagFromApi()
+    {
+        $auth = new Auth();
+        $auth->createAuth();
+
+        factory(Tag::class)->create([
+            'title' => 'Laravel',
+            'slug' => 'laravel',
+            'description' => 'Open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller architectural pattern and based on Symfony.'
+        ]);
+        $blogTag = Tag::first();
+
+        $response = $this->getJson($this->url . '/' . $blogTag->id);
+
+        $response
+            ->assertOk()
+            ->assertJson([
+                'success' => true,
+                'data' => [
+                    'title' => 'Laravel',
+                    'slug' => 'laravel',
+                    'description' => 'Open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller architectural pattern and based on Symfony.'
+                ]
+            ]);
+    }
+
+    /**
+     * Test failed updating an existing record through API
+     *
+     * @return void
+     */
+    public function testFailedUpdateBlogTagFromApi()
+    {
+        $auth = new Auth();
+        $auth->createAuth();
+
+        factory(Tag::class)->create();
+        $blogTag = Tag::first();
+
+        $response = $this->putJson($this->url . '/' . $blogTag->id, [
+            'title' => '',
+            'slug' => '',
+            'description' => ''
+        ]);
+
+        $response
+            ->assertStatus(422)
+            ->assertJson([
+                'success' => false,
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    'title' => ['The title field is required.'],
+                ]
+            ]);
+    }
+
+    /**
+     * Test successfully updating an existing record through API
+     *
+     * @return void
+     */
+    public function testSuccessfullyUpdateBlogTagFromApi()
+    {
+        $auth = new Auth();
+        $auth->createAuth();
+
+        factory(Tag::class)->create();
+        $blogTag = Tag::first();
+
+        $response = $this->putJson($this->url . '/' . $blogTag->id, [
+            'title' => 'Laravel',
+            'slug' => 'laravel',
+            'description' => 'Open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller architectural pattern and based on Symfony.'
+        ]);
+
+        $response
+            ->assertOk()
+            ->assertJson([
+                'success' => true,
+                'message' => 'Data successfully updated.',
+                'data' => [
+                    'title' => 'Laravel',
+                    'slug' => 'laravel',
+                    'description' => 'Open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller architectural pattern and based on Symfony.'
+                ]
+            ]);
+    }
+
+    /**
+     * Test delete a record
+     *
+     * @return void
+     */
+    public function testDeleteBlogTagFromApi()
+    {
+        $auth = new Auth();
+        $auth->createAuth();
+
+        factory(Tag::class)->create();
+        $blogTag = Tag::first();
+
+        $response = $this->deleteJson($this->url, [
+            'selectedData' => [$blogTag->id]
+        ]);
+
+        $response
+            ->assertOk()
+            ->assertJson([
+                'success' => true,
+                'message' => 'Data successfully deleted.'
+            ]);
+
+        $this->assertDeleted($blogTag);
+    }
+}

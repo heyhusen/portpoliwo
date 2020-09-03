8bebdcf1347c6659d15d62a0604b1563502e7b71
Ahmad Husen <husenisme@gmail.com>
2020-09-03 20:49:11 +0700

    Fix blog/category and blog/tag slug on factory

--- database/factories/BlogCategoryFactory.php
+++ database/factories/BlogCategoryFactory.php
@@ -4,11 +4,13 @@
 
 use App\Models\Blog\Category;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(Category::class, function (Faker $faker) {
+    $title = $faker->sentence(2, true);
     return [
-        'title' => $faker->name,
-        'slug' => $faker->domainWord,
-        'description' => $faker->sentence($nbWords = 15, $variableNbWords = true)
+        'title' => $title,
+        'slug' => Str::slug($title, '-'),
+        'description' => $faker->sentence(15, true)
     ];
 });
